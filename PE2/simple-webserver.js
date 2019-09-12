af142cfb7c0c3d8e92bf82b4aba216b09deafe46
jl <johannes.luukkonen@edu.turkuamk.fi>
2019-09-12 01:37:17 -0700

    Second commit: Created web server and depencies.

--- PE2/simple-webserver.js
+++ PE2/simple-webserver.js
@@ -3,4 +3,16 @@ const httpStatus = require("http-status-codes");
 
 const port = 3000;
 
-const app = http.createServer();
+const app = http.createServer((request, response) => {
+    response.writeHead(httpStatus.OK, {
+        "Content-Type" : "text/html"
+    });
+
+    let responseMessage = "<h1>Terve Uljas Maailma!</h1>";
+
+    response.write(responseMessage);
+
+    response.end();
+});
+
+app.listen(port);
